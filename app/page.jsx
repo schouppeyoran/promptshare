aacc643d3631f58bbcf33569293750b8bb1def3b
schouppeyoran <schouppey@gmail.com>
2023-05-12 17:26:44 +0200

    Set up styles & file structure

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+const Home = () => {
+    return (
+        <section className="w-full flex-center flex-col">
+            <h1 className="head_text text-center">
+                Discover & Share
+                <br className="max-md:hidden"/>
+                <span className="orange_gradient text-center"> AI-Powered Prompts</span>
+            </h1>
+            <p className="desc text-center">
+                Promptshare is an open-source AI prompting tool for the modern world which allows users to discover, create and share fine-tuned AI prompts
+            </p>
+        </section>
+    )
+}
+
+export default Home
